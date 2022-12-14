8ba2f0dd57f92a13c9c905a7af07426835cf4489
sandula1999 <120555011+sandula1999@users.noreply.github.com>
2022-12-14 14:07:13 +0530

    Add files via upload

---
+++ checkout.php
@@ -0,0 +1,140 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+};
+
+if(isset($_POST['order'])){
+
+    $name = mysqli_real_escape_string($conn, $_POST['name']);
+    $number = mysqli_real_escape_string($conn, $_POST['number']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $method = mysqli_real_escape_string($conn, $_POST['method']);
+    $placed_on = date('d-M-Y');
+
+    $cart_total = 0;
+    $cart_products[] = '';
+
+    $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+    if(mysqli_num_rows($cart_query) > 0){
+        while($cart_item = mysqli_fetch_assoc($cart_query)){
+            $cart_products[] = $cart_item['name'].' ('.$cart_item['quantity'].') ';
+            $sub_total = ($cart_item['price'] * $cart_item['quantity']);
+            $cart_total += $sub_total;
+        }
+    }
+
+    $total_products = implode(', ',$cart_products);
+
+    $order_query = mysqli_query($conn, "SELECT * FROM `orders` WHERE name = '$name' AND number = '$number' AND email = '$email' AND method = '$method' AND  total_products = '$total_products' AND total_price = '$cart_total'") or die('query failed');
+
+    if($cart_total == 0){
+        $message[] = 'your cart is empty!';
+    }elseif(mysqli_num_rows($order_query) > 0){
+        $message[] = 'order placed already!';
+    }else{
+        mysqli_query($conn, "INSERT INTO `orders`(user_id, name, number, email, method, total_products, total_price, placed_on) VALUES('$user_id', '$name', '$number', '$email', '$method', '$total_products', '$cart_total', '$placed_on')") or die('query failed');
+        mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+        $message[] = 'order placed successfully!';
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>checkout</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php @include 'header.php'; ?>
+
+<section class="heading">
+    <h3>checkout order</h3>
+    <p> <a href="home.php">home</a> / checkout </p>
+</section>
+
+
+
+<section class="checkout">
+
+    <form action="" method="POST">
+
+        <h3>place your order</h3>
+
+        <div class="flex">
+            <div class="inputBox">
+                <span>your name :</span>
+                <input type="text" name="name" placeholder="enter your name">
+            </div>
+            <div class="inputBox">
+                <span>your number :</span>
+                <input type="number" name="number" min="0" placeholder="enter your number">
+            </div>
+            <div class="inputBox">
+                <span>your email :</span>
+                <input type="email" name="email" placeholder="enter your email">
+            </div>
+            <div class="inputBox">
+                <span>payment method :</span>
+                <select name="method">
+                    <option value="cash on delivery">cash on delivery</option>
+                    <option value="credit card">credit card</option>
+                    <option value="paypal">paypal</option>
+                    <option value="paytm">paytm</option>
+                </select>
+            </div>
+            <div class="inputBox">
+                <span>address :</span>
+                <input type="text" name="flat" placeholder="Home Address">
+            </div>
+            
+            <div class="inputBox">
+                <span>city :</span>
+                <input type="text" name="city" placeholder="e.g. Colombo">
+            </div>
+            
+            <div class="inputBox">
+                <span>country :</span>
+                <input type="text" name="country" placeholder="e.g. Sri lanka">
+            </div>
+            <div class="inputBox">
+                <span>postal code :</span>
+                <input type="number" min="0" name="pin_code" placeholder="e.g. 123456">
+            </div>
+        </div>
+
+        <input type="submit" name="order" value="order now" class="btn">
+
+    </form>
+
+</section>
+
+
+
+
+
+
+<?php @include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
